0d9a44fe714d3683fcfb2584d8a64b598315f89b
Maxence Henneron <dernise@live.fr>
2015-06-24 14:45:37 +0200

    Removed useless messages

--- app/controllers/api/containers_controller.rb
+++ app/controllers/api/containers_controller.rb
@@ -9,7 +9,7 @@ class Api::ContainersController < ApplicationController
     if container.save
       render status: 200, json:{container: container}
     else
-      render status: 500, json: {message: 'Couldn\'t save the container', error: container.errors}
+      render status: 500, json: {error: container.errors}
     end
   end
 
@@ -20,7 +20,7 @@ class Api::ContainersController < ApplicationController
       if container.destroyed?
         render :nothing, status: 200
       else
-        render status: 200, json: {message: 'Couldn\'t delete the container', error: container.errors}
+        render status: 200, json: {error: container.errors}
       end
     end
   end
@@ -30,7 +30,7 @@ class Api::ContainersController < ApplicationController
     if container.update_attributes(container_params)
       render status: 200, json: {container: container}
     else
-      render status: 500, json: {message: 'Couldn\'t update the container', error: container.errors}
+      render status: 500, json: {error: container.errors}
     end
   end
 
