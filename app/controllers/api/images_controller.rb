57a977319fa0576986d81c7844ba7d9b5661db38
Maxence Henneron <dernise@live.fr>
2015-06-25 22:45:45 +0200

    Images controller done

---
+++ app/controllers/api/images_controller.rb
@@ -0,0 +1,38 @@
+class Api::ImagesController < ApplicationController
+  def index
+    render json: Image.all
+  end
+
+  def create
+    begin
+      image = Image.new(image_params)
+      image.save ? render(status: 200, json:{ image: image }) : render(status: 500, json: { error: image.errors })
+    rescue ActiveRecord::RecordNotFound => e
+      render status: 500, json: { error:  e }
+    end
+  end
+
+  def delete
+    begin
+      image = Image.find(params[:id])
+      image.destroy
+      image.destroyed? ? render(:nothing, status: 200) : render(status: 200, json: { error: image.errors })
+    rescue ActiveRecord::RecordNotFound => e
+      render status: 500, json: { error:  e }
+    end
+  end
+
+  def update
+    begin
+      image = Image.find(params[:id])
+      image.update_attributes(image_params) ? render(status: 200, json: { image: image }) : render(status: 500, json: { error: image.errors })
+    rescue ActiveRecord::RecordNotFound => e
+      render status: 500, json: { error:  e }
+    end
+  end
+
+  private
+  def image_params
+    params.require(:image).permit(:repo, :name)
+  end
+end
